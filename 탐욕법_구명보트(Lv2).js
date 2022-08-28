b6fcfaea31cfaf0576aff3e28f70296c8bec9b48
jinhee oh <exojj.com.gmail.com>
2022-08-28 23:05:57 +0900

    구명보트 나의 3번째풀이와 다른사람 풀이 추가

--- 탐욕법_구명보트(Lv2).js
+++ 탐욕법_구명보트(Lv2).js
@@ -37,3 +37,41 @@ function solution(people, limit) {
    return answer;
 }
 // 테스트 케이스만 통과하고 최종은 통과하지 못했다. ㅠㅠ
+
+
+// 나의 3번째 풀이
+function solution(people, limit) {
+   let answer = 0;
+   people = people.sort((a,b)=> {return a-b});
+   
+   let j = 1;
+   let k = 0; 
+   for(let i = 0; i < people.length; i++){
+      if(people[i+k] + people[i+j] <= limit){
+         j++;
+         k++; 
+      }
+      answer++; 
+      if(i+k === people.length - 1){ break; }   
+   }
+  
+   return answer;
+}
+// 테스트 케이스만 통과하고 최종은 통과하지 못했다. 아진짜 이풀이도 맞는데 왜 통과를 못하냐..ㅠㅠ진짜...
+// 왜 틀린지 모르겠다.ㅠㅠ
+
+
+// 다른사람의 풀이.
+function solution(people, limit) {
+   let answer = 0;
+   people = people.sort((a,b)=>b-a);
+
+   for(let i =0, j = people.length-1; i <= j ; i++ ){
+       if(people[i]+people[j] <= limit) j--
+       answer++
+   }
+
+   return answer;
+}
+
+// 이사람 풀이도 이해가 되는데 왜 내풀이는 안되는 걸까??.....
