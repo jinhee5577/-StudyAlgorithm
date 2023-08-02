b6104e4af8d13af14615d1064ea4726c47182d60
ohjinhee <riricos153@naver.com>
2023-08-02 17:05:56 +0900

    JadenCase 문자열 만들기 2번째풀이

--- JadenCase_문자열만들기(Lv2).js
+++ JadenCase_문자열만들기(Lv2).js
@@ -12,3 +12,17 @@ function solution(s) {
 }
 
 // 테스트 케이스들은 통과 하였지만, 최종제출에서 런타임에러로 실패함. (왜 시간이 오래걸리는거지?....)
+
+
+
+// 나의 2번째풀이
+function solution (s){
+    let answer = s.split(' ').map((st) => {
+        st = st.toLowerCase();
+        return st.charAt(0).toUpperCase() + st.substring(1);
+    });
+    
+    return answer.join(' ');
+    // charAt(0) 문자열의 0번째를 선택
+    // substring(1) 문자열 1번부터 끝까지
+}
